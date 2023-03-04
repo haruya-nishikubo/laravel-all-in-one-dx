cf0c53623b6dc277a658629c60147ff03b802aa8
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-03-04 17:29:22 +0900

    make: layouts

---
+++ files/resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div class="flex items-center justify-between py-12">
+    <ul>
+        <li>
+            <x-admin-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-admin-nav-link>
+        </li>
+
+        <li class="mt-4">
+            <x-admin-nav-label>アクセス管理</x-admin-nav-label>
+        </li>
+        <ul class="ml-4">
+            @if(auth()->user()->isRouteAllowed('admin.route-role.index'))
+            <li>
+                <x-admin-nav-link :href="route('admin.route-role.index')" :active="request()->routeIs('admin.route-role.*')">
+                    {{ __('models.route_role.table_name') }}
+                </x-admin-nav-link>
+            </li>
+            @endif
+
+            @if(auth()->user()->isRouteAllowed('admin.route-policy.index'))
+            <li>
+                <x-admin-nav-link :href="route('admin.route-policy.index')" :active="request()->routeIs('admin.route-policy.*')">
+                    {{ __('models.route_policy.table_name') }}
+                </x-admin-nav-link>
+            </li>
+            @endif
+        </ul>
+    </ul>
+</div>
