ca8b839d08aa9c3f3b9ee1dde5c9ecc5a3aea514
Haruya Nishikubo <91885194+haruya-nishikubo@users.noreply.github.com>
2023-05-13 16:55:13 +0900

    レイアウト変更 (#2)
    * change layout on index
    
    * fix layout

--- files/resources/views/admin/route_role/index.blade.php
+++ files/resources/views/admin/route_role/index.blade.php
@@ -7,6 +7,64 @@
 
     <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{-- list --}}
+            <div class="py-4">
+                <x-card.default>
+                    <x-card.header>{{ __('actions.index') }}</x-card.header>
+                    <table class="table-auto w-full">
+                        <thead>
+                        @if (auth()->user()->isRouteAllowed('admin.route_role.export'))
+                            <tr>
+                                <x-tables.th class="text-right" colspan="2">
+                                    <x-links.button-info href="{{ route('admin.route_role.export', array_merge($criteria, ['encoding' => 'sjis'])) }}">
+                                        <span class="material-icons align-middle">file_download</span>
+                                        <span>{{ __('actions.export') }}(win)</span>
+                                    </x-links.button-info>
+                                    <x-links.button-info href="{{ route('admin.route_role.export', $criteria) }}">
+                                        <span class="material-icons align-middle">file_download</span>
+                                        <span>{{ __('actions.export') }}(mac)</span>
+                                    </x-links.button-info>
+                                </x-tables.th>
+                            </tr>
+                        @endif
+
+                        <tr>
+                            <x-tables.th>{{ __('models.route_role.field.name') }}</x-tables.th>
+                            <x-tables.th class="text-right">
+                                @if (auth()->user()->isRouteAllowed('admin.route_role.create'))
+                                    <x-links.button-info href="{{ route('admin.route_role.create') }}">
+                                        <span class="material-icons align-middle">create</span>
+                                        <span>{{ __('actions.create') }}</span>
+                                    </x-links.button-info>
+                                @endif
+                            </x-tables.th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach($route_roles as $route_role)
+                            <tr>
+                                <x-tables.td>{{ $route_role->name }}</x-tables.td>
+                                <x-tables.td class="text-right">
+                                    @if (auth()->user()->isRouteAllowed('admin.route_role.show'))
+                                        <x-links.button-default href="{{ route('admin.route_role.show', $route_role) }}">
+                                            <span class="material-icons align-middle">read_more</span>
+                                            <span>{{ __('actions.show') }}</span>
+                                        </x-links.button-default>
+                                    @endif
+                                </x-tables.td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+
+                    <div class="mt-4">
+                        {{ $route_roles->appends($criteria)->links() }}
+                    </div>
+                </x-card.default>
+            </div>
+
+            {{-- search --}}
             <div class="py-4">
                 <x-card.default>
                     <x-card.header>{{ __('actions.search') }}</x-card.header>
@@ -39,82 +97,29 @@
                 </x-card.default>
             </div>
 
-            <x-card.default>
-                <x-card.header>{{ __('actions.index') }}</x-card.header>
-                <table class="table-auto w-full">
-                    <thead>
-                        @if (auth()->user()->isRouteAllowed('admin.route_role.export'))
-                        <tr>
-                            <x-tables.th class="text-right" colspan="2">
-                                <x-links.button-info href="{{ route('admin.route_role.export', array_merge($criteria, ['encoding' => 'sjis'])) }}">
-                                    <span class="material-icons align-middle">file_download</span>
-                                    <span>{{ __('actions.export') }}(win)</span>
-                                </x-links.button-info>
-                                <x-links.button-info href="{{ route('admin.route_role.export', $criteria) }}">
-                                    <span class="material-icons align-middle">file_download</span>
-                                    <span>{{ __('actions.export') }}(mac)</span>
-                                </x-links.button-info>
-                            </x-tables.th>
-                        </tr>
-                        @endif
-
-                        <tr>
-                            <x-tables.th>{{ __('models.route_role.field.name') }}</x-tables.th>
-                            <x-tables.th class="text-right">
-                                @if (auth()->user()->isRouteAllowed('admin.route_role.create'))
-                                <x-links.button-info href="{{ route('admin.route_role.create') }}">
-                                    <span class="material-icons align-middle">create</span>
-                                    <span>{{ __('actions.create') }}</span>
-                                </x-links.button-info>
-                                @endif
-                            </x-tables.th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                    @foreach($route_roles as $route_role)
-                        <tr>
-                            <x-tables.td>{{ $route_role->name }}</x-tables.td>
-                            <x-tables.td class="text-right">
-                                @if (auth()->user()->isRouteAllowed('admin.route_role.show'))
-                                <x-links.button-default href="{{ route('admin.route_role.show', $route_role) }}">
-                                    <span class="material-icons align-middle">read_more</span>
-                                    <span>{{ __('actions.show') }}</span>
-                                </x-links.button-default>
-                                @endif
-                            </x-tables.td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-
-                <div class="mt-4">
-                    {{ $route_roles->appends($criteria)->links() }}
-                </div>
-            </x-card.default>
-
+            {{-- import --}}
             @if (auth()->user()->isRouteAllowed('admin.route_role.import'))
-                <div class="py-4">
-                    <x-card.default>
-                        <x-card.header>{{ __('actions.import') }}</x-card.header>
-                        <form action="{{ route('admin.route_role.import') }}" method="POST" enctype="multipart/form-data" class="mt-4">
-                            @csrf
+            <div class="py-4">
+                <x-card.default>
+                    <x-card.header>{{ __('actions.import') }}</x-card.header>
+                    <form action="{{ route('admin.route_role.import') }}" method="POST" enctype="multipart/form-data" class="mt-4">
+                        @csrf
 
-                            <div>
-                                <x-forms.label :required="true" title="CSV">
-                                    <x-forms.input type="file" name="source" required="required" />
-                                </x-forms.label>
-                            </div>
+                        <div>
+                            <x-forms.label :required="true" title="CSV">
+                                <x-forms.input type="file" name="source" required="required" />
+                            </x-forms.label>
+                        </div>
 
-                            <div class="flex justify-end mt-4">
-                                <x-forms.submit-success>
-                                    <span class="material-icons align-middle">file_upload</span>
-                                    <span>{{ __('actions.import') }}</span>
-                                </x-forms.submit-success>
-                            </div>
-                        </form>
-                    </x-card.default>
-                </div>
+                        <div class="flex justify-end mt-4">
+                            <x-forms.submit-success>
+                                <span class="material-icons align-middle">file_upload</span>
+                                <span>{{ __('actions.import') }}</span>
+                            </x-forms.submit-success>
+                        </div>
+                    </form>
+                </x-card.default>
+            </div>
             @endif
 
         </div>
