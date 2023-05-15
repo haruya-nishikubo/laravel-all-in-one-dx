9db53f8f41ed6be6e79f7578ece6ce0b38cd4d03
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-05-15 10:29:50 +0900

    fix class name

--- src/Models/RouteRole.php
+++ src/Models/RouteRole.php
@@ -23,7 +23,7 @@ class RouteRole extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(\App\Models\User::class);
     }
 
     protected static function newFactory()
