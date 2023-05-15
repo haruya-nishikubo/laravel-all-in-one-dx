a25e5c20d2181fe4107ae09f7334148eb31db9cb
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-05-15 10:40:27 +0900

    bugfix: namespace

--- files/app/Http/Controllers/Admin/RoutePolicy/ImportController.php
+++ files/app/Http/Controllers/Admin/RoutePolicy/ImportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin\RoutePolicy;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\RoutePolicy\ImportRequest;
-use HaruyaNishikubo\AllInOneDx\UploadFileInfo;
+use HaruyaNishikubo\AllInOneDx\Models\UploadFileInfo;
 use HaruyaNishikubo\AllInOneDx\Models\RoutePolicy;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
