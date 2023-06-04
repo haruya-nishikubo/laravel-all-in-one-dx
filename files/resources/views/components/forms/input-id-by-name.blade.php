f9a7768bd63991c69745b8a71aa0501be9feb092
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-04 20:01:08 +0900

    component: input-id-by-name

---
+++ files/resources/views/components/forms/input-id-by-name.blade.php
@@ -0,0 +1,25 @@
+<div x-data="{
+    sources: {{ $sources }},
+    selectedName: '',
+    selectedId: null,
+    setId() {
+        const selectedSource = this.sources.find(source => source.name == this.selectedName);
+        this.selectedId = selectedSource ? selectedSource.id : null;
+        }
+    }">
+    <x-forms.input type="text"
+                   name="{{ $inputFieldName ?? '' }}"
+                   list="{{ $dataListId ?? 'sources' }}"
+                   x-model="selectedName"
+                   @input="setId()"
+                   placeholder="{{ $placeholder ?? '' }}"
+                   required />
+    <input type="hidden"
+           name="{{ $hiddenFieldName }}"
+           x-model="selectedId" />
+    <datalist id="{{ $dataListId ?? 'sources' }}">
+        <template x-for="source in sources">
+            <option :value="source.name"></option>
+        </template>
+    </datalist>
+</div>
