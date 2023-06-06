9f7e4f6459822eb1b6ed4c133add0b5d083b364a
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-07 07:11:48 +0900

    tailwind config color

---
+++ files/tailwind-config/color.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'primary': '#3B71CA',
+    'secondary': '#9FA6B2',
+    'success': '#14A44D',
+    'danger': '#DC4C64',
+    'warning': '#E4A11B',
+    'info': '#54B4D3',
+    'light': '#F9FAFB',
+    'dark': '#1F2937',
+};
