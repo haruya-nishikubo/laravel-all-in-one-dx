eb6fe76eec6b28feebca74e9113c5b74b18f570f
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-07 08:15:27 +0900

    fix colors

---
+++ files/tailwind-config/colors.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    'primary': colors.blue,
+    'secondary': colors.neutral,
+    'success': colors.green,
+    'danger': colors.red,
+    'warning': colors.yellow,
+    'info': colors.sky,
+    'light': colors.white,
+    'dark': colors.gray,
+};
