b3ed423ba8e6c0569e14feb1679fdce0f92ad8a8
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-07 08:50:58 +0900

    badge

---
+++ files/resources/views/components/badge/danger.blade.php
@@ -0,0 +1,1 @@
+<span class="bg-danger-100 text-danger-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-danger-900 dark:text-danger-300">{{ $slot }}</span>
---
+++ files/resources/views/components/badge/default.blade.php
@@ -0,0 +1,1 @@
+<span class="bg-secondary-100 text-secondary-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-secondary-900 dark:text-secondary-300">{{ $slot }}</span>
