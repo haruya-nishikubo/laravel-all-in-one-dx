c7182933b09f7386b7c85780cbe9e16e83c81ba4
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-07 08:59:01 +0900

    remove darkmode

--- files/resources/views/components/badge/info.blade.php
+++ files/resources/views/components/badge/info.blade.php
@@ -1,1 +1,1 @@
-<span class="bg-info-100 text-info-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-info-900 dark:text-info-300">{{ $slot }}</span>
+<span class="bg-info-100 text-info-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded">{{ $slot }}</span>
