fdc429de80f354188ed0c933883dad687b6c21d4
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-07 09:02:36 +0900

    remove dark

--- files/resources/views/components/badge/warning.blade.php
+++ files/resources/views/components/badge/warning.blade.php
@@ -1,1 +1,1 @@
-<span class="bg-warning-100 text-warning-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-warning-900 dark:text-warning-300">{{ $slot }}</span>
+<span class="bg-warning-100 text-warning-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded">{{ $slot }}</span>
