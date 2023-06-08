8305fb37307c7b20158f1e2a959744f1a699c322
Haruya Nishikubo <91885194+haruya-nishikubo@users.noreply.github.com>
2023-06-08 14:58:11 +0900

    fix light (#23)

--- files/resources/views/components/links/button-default.blade.php
+++ files/resources/views/components/links/button-default.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->class(['inline-block px-6 py-3 rounded-md font-medium tracking-wide bg-dark-600 text-light hover:bg-dark-500']) }}>{{ $slot }}</a>
+<a {{ $attributes->class(['inline-block px-6 py-3 rounded-md font-medium tracking-wide bg-dark-600 text-light-100 hover:bg-dark-500']) }}>{{ $slot }}</a>
--- files/tailwind-config/colors.js
+++ files/tailwind-config/colors.js
@@ -1,12 +1,22 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    'primary': colors.blue,
-    'secondary': colors.neutral,
-    'success': colors.green,
-    'danger': colors.red,
-    'warning': colors.yellow,
-    'info': colors.sky,
-    'light': colors.white,
-    'dark': colors.gray,
+    'primary': colors.purple, // Primary as Purple
+    'secondary': colors.blue, // Secondary as Blue
+    'info': colors.sky, // Info as Sky
+    'success': colors.cyan,  // Success as Cyan
+    'warning': colors.yellow, // Warning as Yellow
+    'danger': colors.red, // Danger as Red
+    'light': colors.gray, // Light as Gray
+    'dark': {
+        '100': '#F5F5F5', // Light Gray
+        '200': '#E5E5E5', // Slightly Darker Gray
+        '300': '#D4D4D4', // Even more Dark Gray
+        '400': '#A3A3A3',
+        '500': '#737373', // Dark Gray
+        '600': '#525252',
+        '700': '#404040', // Even more Dark Gray
+        '800': '#262626', // Slightly Darker Gray
+        '900': '#171717', // Dark Gray
+    },
 };
