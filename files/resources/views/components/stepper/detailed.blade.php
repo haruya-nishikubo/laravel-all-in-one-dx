a03e88663a13f622ba826742961080409d082afe
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-08 14:15:50 +0900

    grid

--- files/resources/views/components/stepper/detailed.blade.php
+++ files/resources/views/components/stepper/detailed.blade.php
@@ -1,4 +1,5 @@
-<ol class="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+<div class="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+    <ul class="grid grid-cols-3 gap-4">
     @foreach($items as $item)
         <li @class(['flex items-center text-info-600 space-x-2.5', 'text-info-600' => $item['is_active'], 'text-dark-400' => (! $item['is_active'])])>
             <span @class(['flex items-center justify-center w-8 h-8 border border-info-600 rounded-full shrink-0', 'border-info-600' => $item['is_active'], 'border-dark-500' => (! $item['is_active'])])>
@@ -10,4 +11,5 @@
             </span>
         </li>
     @endforeach
-</ol>
+    </ul>
+</div>
