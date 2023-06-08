1ee47ad2269749016dcc79bf44e848318626d906
Haruya Nishikubo <91885194+haruya-nishikubo@users.noreply.github.com>
2023-06-08 15:23:09 +0900

    fix sidebar (#24)

--- files/resources/views/components/sidebar/link.blade.php
+++ files/resources/views/components/sidebar/link.blade.php
@@ -1,4 +1,4 @@
 @props(['active'])
-<a {{ $attributes->class(['flex items-center px-4 py-2 rounded-md', 'text-dark-600 bg-dark-200' => $active, 'text-dark-100 hover:bg-dark-200 hover:text-dark-600' => ! $active]) }}>
+<a {{ $attributes->class(['flex items-center px-4 py-2 rounded-md', 'text-dark-600 bg-primary-300' => $active, 'text-dark-600 hover:bg-primary-200' => ! $active]) }}>
     <span class="mx-4 font-medium">{{ $slot }}</span>
 </a>
