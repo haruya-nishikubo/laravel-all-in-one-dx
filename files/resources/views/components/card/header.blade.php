380aee8e170ef55014a3f02c3d4619d377ff789d
Haruya Nishikubo <haruya.nishikubo@gmail.com>
2023-06-10 10:01:16 +0900

    fix card header

--- files/resources/views/components/card/header.blade.php
+++ files/resources/views/components/card/header.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->class('-mt-8 mx-4 py-4 rounded-md bg-dark-400') }}>
+<div {{ $attributes->class('-mt-8 mx-4 py-4 rounded-md bg-primary-400') }}>
     <h6 class="pl-4 font-bold text-light-100">{{ $slot }}</h6>
 </div>
