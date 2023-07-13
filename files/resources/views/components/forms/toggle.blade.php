32e232089eae199ab8917591ce82aee94c2f0989
Haruya Nishikubo <91885194+haruya-nishikubo@users.noreply.github.com>
2023-07-13 12:14:39 +0900

    チェックボックスに disabled を追加 (#30)

--- files/resources/views/components/forms/toggle.blade.php
+++ files/resources/views/components/forms/toggle.blade.php
@@ -1,6 +1,6 @@
 <label class="relative inline-flex items-center cursor-pointer">
     <input type="hidden" name="{{ $attributes->get('name') }}" value="{{ $no ?? 0 }}" />
-    <input type="checkbox" name="{{ $attributes->get('name') }}"  value="{{ $yes ?? 1 }}" class="sr-only peer" @checked($checked)>
+    <input type="checkbox" name="{{ $attributes->get('name') }}"  value="{{ $yes ?? 1 }}" class="sr-only peer" @checked($checked ?? false) @disabled($disabled ?? false)>
     <div class="w-12 h-6 bg-dark-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-info-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-light-100 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-light-100 after:border-dark-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-info-600"></div>
     <span class="ml-4 text-sm font-bold">{{ $slot }}</span>
 </label>
